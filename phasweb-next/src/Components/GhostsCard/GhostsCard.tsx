e708a313bf86af59022245b428ac6182cd09381b
pgsleepy <delano.schild98@gmail.com>
2023-04-07 05:25:11 +0200

    Cards addition update!

---
+++ phasweb-next/src/Components/GhostsCard/GhostsCard.tsx
@@ -0,0 +1,315 @@
+export default function EvidenceCard() {
+  return (
+    <div className="card w-150 bg-base-300 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">Phasmophobia Ghosts</h2>
+        <p className="text-xs">
+          The ghosts have different abilities and such!
+          <br />
+          So make sure to read through them to get a better understanding of the
+          ghosts!
+        </p>
+        <div className="divider" />
+
+        <div
+          className="Ghosts overflow-auto no-scrollbar"
+          style={{ maxHeight: "36rem", overflowX: "hidden" }}
+        >
+          <div className="Deogen">
+            <div className="card bg-base-100 p-5">
+              <div className="card-actions justify-end">
+                <button className="btn btn-square btn-sm absolute">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+
+              <div className="card-title flex items-center">
+                <a href="/">Deogen</a>
+                <div className="flex flex-row gap-2 absolute ml-20 mb-1.5">
+                  <div className="badge badge-primary badge-outline mt-2 text-xs">
+                    D.O.T.S
+                  </div>
+                  <div className="badge badge-secondary badge-outline mt-2 text-xs">
+                    WRITING
+                  </div>
+                  <div className="badge badge-accent badge-outline mt-2 text-xs">
+                    SPIRIT BOX
+                  </div>
+                </div>
+              </div>
+              <div className="text-xs">
+                A deogen is a speed demon when far, a snail demon when near!
+              </div>
+
+              <div className="divider" />
+              <div className="flex m-auto">
+                <div className="flex flex-row">
+                  <div className="card w-25 ">
+                    <p className="text-xm">Identifier</p>
+                    <p className="text-xs">
+                      &bull; Very fast when far. <br />
+                      &bull; Very slow when near. <br />
+                      &bull; <b>Always</b> knows where you are.
+                      <br />
+                    </p>
+                  </div>
+
+                  <div className="divider divider-horizontal"></div>
+
+                  <div className="card w-25">
+                    <p className="text-xm">Properties</p>
+                    <div className="text-xs">
+                      <p className="absolute">
+                        <u>Speed</u>
+                      </p>{" "}
+                      <br />
+                      &bull; Above 6 meters: <b>3 m/s</b>
+                      <br />
+                      &bull; Below 2.5 meters: <b>0.4 m/s</b>
+                      <br />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <br />
+
+          <div className="Deogen">
+            <div className="card bg-base-100 p-5">
+              <div className="card-actions justify-end">
+                <button className="btn btn-square btn-sm absolute">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+
+              <div className="card-title flex items-center">
+                <a href="/">Deogen</a>
+                <div className="flex flex-row gap-2 absolute ml-20 mb-1.5">
+                  <div className="badge badge-primary badge-outline mt-2 text-xs">
+                    D.O.T.S
+                  </div>
+                  <div className="badge badge-secondary badge-outline mt-2 text-xs">
+                    WRITING
+                  </div>
+                  <div className="badge badge-accent badge-outline mt-2 text-xs">
+                    SPIRIT BOX
+                  </div>
+                </div>
+              </div>
+              <div className="text-xs">
+                A deogen is a speed demon when far, a snail demon when near!
+              </div>
+
+              <div className="divider" />
+              <div className="flex m-auto">
+                <div className="flex flex-row">
+                  <div className="card w-25 ">
+                    <p className="text-xm">Identifier</p>
+                    <p className="text-xs">
+                      &bull; Very fast when far. <br />
+                      &bull; Very slow when near. <br />
+                      &bull; <b>Always</b> knows where you are.
+                      <br />
+                    </p>
+                  </div>
+
+                  <div className="divider divider-horizontal"></div>
+
+                  <div className="card w-25">
+                    <p className="text-xm">Properties</p>
+                    <div className="text-xs">
+                      <p className="absolute">
+                        <u>Speed</u>
+                      </p>{" "}
+                      <br />
+                      &bull; Above 6 meters: <b>3 m/s</b>
+                      <br />
+                      &bull; Below 2.5 meters: <b>0.4 m/s</b>
+                      <br />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <br />
+
+          <div className="Deogen">
+            <div className="card bg-base-100 p-5">
+              <div className="card-actions justify-end">
+                <button className="btn btn-square btn-sm absolute">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+
+              <div className="card-title flex items-center">
+                <a href="/">Deogen</a>
+                <div className="flex flex-row gap-2 absolute ml-20 mb-1.5">
+                  <div className="badge badge-primary badge-outline mt-2 text-xs">
+                    D.O.T.S
+                  </div>
+                  <div className="badge badge-secondary badge-outline mt-2 text-xs">
+                    WRITING
+                  </div>
+                  <div className="badge badge-accent badge-outline mt-2 text-xs">
+                    SPIRIT BOX
+                  </div>
+                </div>
+              </div>
+              <div className="text-xs">
+                A deogen is a speed demon when far, a snail demon when near!
+              </div>
+
+              <div className="divider" />
+              <div className="flex m-auto">
+                <div className="flex flex-row">
+                  <div className="card w-25 ">
+                    <p className="text-xm">Identifier</p>
+                    <p className="text-xs">
+                      &bull; Very fast when far. <br />
+                      &bull; Very slow when near. <br />
+                      &bull; <b>Always</b> knows where you are.
+                      <br />
+                    </p>
+                  </div>
+
+                  <div className="divider divider-horizontal"></div>
+
+                  <div className="card w-25">
+                    <p className="text-xm">Properties</p>
+                    <div className="text-xs">
+                      <p className="absolute">
+                        <u>Speed</u>
+                      </p>{" "}
+                      <br />
+                      &bull; Above 6 meters: <b>3 m/s</b>
+                      <br />
+                      &bull; Below 2.5 meters: <b>0.4 m/s</b>
+                      <br />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <br />
+
+          <div className="Deogen">
+            <div className="card bg-base-100 p-5">
+              <div className="card-actions justify-end">
+                <button className="btn btn-square btn-sm absolute">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+
+              <div className="card-title flex items-center">
+                <a href="/">Deogen</a>
+                <div className="flex flex-row gap-2 absolute ml-20 mb-1.5">
+                  <div className="badge badge-primary badge-outline mt-2 text-xs">
+                    D.O.T.S
+                  </div>
+                  <div className="badge badge-secondary badge-outline mt-2 text-xs">
+                    WRITING
+                  </div>
+                  <div className="badge badge-accent badge-outline mt-2 text-xs">
+                    SPIRIT BOX
+                  </div>
+                </div>
+              </div>
+              <div className="text-xs">
+                A deogen is a speed demon when far, a snail demon when near!
+              </div>
+
+              <div className="divider" />
+              <div className="flex m-auto">
+                <div className="flex flex-row">
+                  <div className="card w-25 ">
+                    <p className="text-xm">Identifier</p>
+                    <p className="text-xs">
+                      &bull; Very fast when far. <br />
+                      &bull; Very slow when near. <br />
+                      &bull; <b>Always</b> knows where you are.
+                      <br />
+                    </p>
+                  </div>
+
+                  <div className="divider divider-horizontal"></div>
+
+                  <div className="card w-25">
+                    <p className="text-xm">Properties</p>
+                    <div className="text-xs">
+                      <p className="absolute">
+                        <u>Speed</u>
+                      </p>{" "}
+                      <br />
+                      &bull; Above 6 meters: <b>3 m/s</b>
+                      <br />
+                      &bull; Below 2.5 meters: <b>0.4 m/s</b>
+                      <br />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
