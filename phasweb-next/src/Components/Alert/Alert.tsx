b76dc5e4a0694eee3f0dbae9b781919ab7f0fa40
pgsleepy <delano.schild98@gmail.com>
2023-05-12 22:20:10 +0200

    Added new icon and left side

--- phasweb-next/src/Components/Alert/Alert.tsx
+++ phasweb-next/src/Components/Alert/Alert.tsx
@@ -1,22 +1,15 @@
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 export default function Alert() {
   return (
-    <div className="alert alert-info shadow-lg absolute text-right pt-5 z-50 w-72 right-1 top-20">
+    <div className="alert alert-info bg-secondary shadow-lg absolute text-left z-50 w-72 left-4 top-20">
       <div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          className="stroke-current flex-shrink-0 w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          ></path>
-        </svg>
+        <FontAwesomeIcon
+          icon={faCircleExclamation}
+          style={{ width: "1.5rem", height: "1.5rem" }}
+        />
         <div>
           <h3 className="font-bold">Thanks for testing!</h3>
           <div className="text-xs">
